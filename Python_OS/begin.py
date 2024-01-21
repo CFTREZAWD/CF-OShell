68a65a5ac1c059e604e04b630660fb839ba38c2d
CFTREZAWD <153073736+CFTREZAWD@users.noreply.github.com>
2024-01-21 19:57:01 +0100

    Update begin.py

--- Python_OS/begin.py
+++ Python_OS/begin.py
@@ -4,7 +4,7 @@ import CF_OShell
 
 def boot():
     print("Welcome to CF-OShell. The worst shell ever created ! ")
-    time.sleep(0.5)
+    time.sleep(2)
     print("Booting...")
 
 if __name__ == "__main__":
